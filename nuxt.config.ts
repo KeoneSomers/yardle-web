07f6fe20212276feaef7319d62107ec66d9d1b17
KeoneSomers <keone.somers@outlook.com>
2022-11-29 09:49:07 +0000

    added use head for meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,16 @@ export default defineNuxtConfig({
     typescript: {
         shim: false,
     },
+    app: {
+        head: {
+            title: "Yardle - Yard Management App",
+            meta: [
+                {
+                    name: "description",
+                    content: "A website to help you with your yard.",
+                },
+                { name: "author", content: "Keone Somers" },
+            ],
+        },
+    },
 });
--- pages/yards/[id].vue
+++ pages/yards/[id].vue
@@ -1,4 +1,9 @@
 <script setup>
+    useHead({
+        title: "Test Page",
+        meta: [{ name: "description", content: "test page desc" }],
+    });
+
     const { id } = useRoute().params;
 
     // use the following code to send the user to the error page
