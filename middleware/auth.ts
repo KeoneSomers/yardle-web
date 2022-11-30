edf75263fb4074461d3eabbbcf75876bb9de350e
KeoneSomers <keone.somers@outlook.com>
2022-11-30 16:12:24 +0000

    commented out middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,23 +1,22 @@
-export default defineNuxtRouteMiddleware(async (to, from) => {
+export default defineNuxtRouteMiddleware((to) => {
     const user = useSupabaseUser();
 
-    const client = useSupabaseAuthClient();
-    const { data, error } = await client.auth.getSession();
+    console.log("Hit middleware!");
+    console.log(user.value);
 
-    console.log("session is: ");
-    console.log(data.session);
-
-    if (to.path != "/login" && to.path != "/signup") {
-        // trying to access a protected page
-        // - must be logged in
-        if (!data.session) {
-            return navigateTo("/login");
-        }
-    } else if (to.path == "/login" || to.path == "/signup") {
-        // trying to access login or signup page
-        // - must be logged out
-        if (data.session) {
-            return navigateTo("/");
-        }
-    }
+    // if (to.path != "/login" && to.path != "/signup") {
+    //     // trying to access a protected page
+    //     // - must be logged in
+    //     if (!user.value) {
+    //         return navigateTo("/login");
+    //     }
+    // } else if (to.path == "/login" || to.path == "/signup") {
+    //     // trying to access login or signup page
+    //     // - must be logged out
+    //     if (user.value) {
+    //         return navigateTo("/");
+    //     }
+    // } else {
+    //     return navigateTo("/login");
+    // }
 });
