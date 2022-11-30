a639039a96490d093bdd96113f11a47a64a60858
KeoneSomers <keone.somers@outlook.com>
2022-11-30 12:08:21 +0000

    minor

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -6,12 +6,16 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     if (to.path != "/login" && to.path != "/signup") {
         // trying to access a protected page
         // - must be logged in
+        console.log("trying to access a protected page: " + to.path);
+        console.log(user.value);
         if (!user.value) {
             return navigateTo("/login");
         }
     } else if (to.path == "/login" || to.path == "/signup") {
         // trying to access login or signup page
         // - must be logged out
+        console.log("trying to access login or signup page: " + to.path);
+        console.log(user.value);
         if (user.value) {
             return navigateTo("/");
         }
