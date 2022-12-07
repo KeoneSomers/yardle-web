32726c913e1072bb9e4727a78c5ea25c19ff6e7d
KeoneSomers <keone.somers@outlook.com>
2022-12-07 11:01:36 +0000

    simplified layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,5 @@
 <script setup>
-    import { ref } from "vue";
-    import {
-        Dialog,
-        DialogPanel,
-        Menu,
-        MenuButton,
-        MenuItem,
-        MenuItems,
-        TransitionChild,
-        TransitionRoot,
-    } from "@headlessui/vue";
-    import {
-        Bars3BottomLeftIcon,
-        BellIcon,
-        HomeIcon,
-        InboxIcon,
-        UsersIcon,
-        XMarkIcon,
-        Cog6ToothIcon,
-    } from "@heroicons/vue/24/outline/index.js";
+    import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
 
     const supabase = useSupabaseAuthClient();
     const client = useSupabaseClient();
@@ -48,257 +29,111 @@
     };
 
     const navigation = [
-        { name: "Dashboard", to: "/", icon: HomeIcon },
-        { name: "Members", to: "/crud", icon: UsersIcon },
-        { name: "Settings", to: "/crud", icon: Cog6ToothIcon },
+        { name: "Dashboard", to: "/" },
+        { name: "Members", to: "/crud" },
+        { name: "Settings", to: "/crud" },
     ];
 
     const userNavigation = [
         { name: "Your Profile", to: "/profile" },
         { name: "Manage Yards", to: "/" },
     ];
-
-    const sidebarOpen = ref(false);
 </script>
 
 <template>
     <div>
-        <!-- Mobile Sidebar -->
-        <TransitionRoot as="template" :show="sidebarOpen">
-            <Dialog
-                as="div"
-                class="relative z-40 md:hidden"
-                @close="sidebarOpen = false"
-            >
-                <TransitionChild
-                    as="template"
-                    enter="transition-opacity ease-linear duration-300"
-                    enter-from="opacity-0"
-                    enter-to="opacity-100"
-                    leave="transition-opacity ease-linear duration-300"
-                    leave-from="opacity-100"
-                    leave-to="opacity-0"
-                >
-                    <div class="fixed inset-0 bg-gray-600 bg-opacity-75" />
-                </TransitionChild>
-
-                <div class="fixed inset-0 z-40 flex">
-                    <TransitionChild
-                        as="template"
-                        enter="transition ease-in-out duration-300 transform"
-                        enter-from="-translate-x-full"
-                        enter-to="translate-x-0"
-                        leave="transition ease-in-out duration-300 transform"
-                        leave-from="translate-x-0"
-                        leave-to="-translate-x-full"
-                    >
-                        <DialogPanel
-                            class="relative flex w-full max-w-xs flex-1 flex-col bg-indigo-700 pt-5 pb-4"
-                        >
-                            <TransitionChild
-                                as="template"
-                                enter="ease-in-out duration-300"
-                                enter-from="opacity-0"
-                                enter-to="opacity-100"
-                                leave="ease-in-out duration-300"
-                                leave-from="opacity-100"
-                                leave-to="opacity-0"
+        <div class="bg-gray-900 p-4">
+            <header>
+                <nav>
+                    <div class="flex justify-between items-center">
+                        <div class="flex items-center space-x-4">
+                            <div class="text-yellow-500 text-xl">Yardle</div>
+                            <NuxtLink
+                                v-if="user.user_metadata.selected_yard"
+                                v-for="item in navigation"
+                                :key="item.name"
+                                :to="item.to"
+                                :class="[
+                                    item.to == router.currentRoute.value.path
+                                        ? 'bg-indigo-800 text-white'
+                                        : 'text-indigo-100 hover:bg-indigo-600',
+                                    'p-2 text-sm font-medium rounded-md',
+                                ]"
                             >
-                                <div class="absolute top-0 right-0 -mr-12 pt-2">
-                                    <button
-                                        type="button"
-                                        class="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                                        @click="sidebarOpen = false"
+                                {{ item.name }}
+                            </NuxtLink>
+                        </div>
+
+                        <div class="flex items-center text-white space-x-4">
+                            <Menu v-if="profile" as="div" class="relative ml-3">
+                                <div>
+                                    <MenuButton
+                                        class="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                                     >
                                         <span class="sr-only"
-                                            >Close sidebar</span
+                                            >Open user menu</span
                                         >
-                                        <XMarkIcon
-                                            class="h-6 w-6 text-white"
-                                            aria-hidden="true"
+                                        <SupabaseImage
+                                            v-if="profile.avatar_url"
+                                            id="avatars"
+                                            :path="profile.avatar_url"
+                                            class="w-8 h-8 rounded-full overflow-hidden"
                                         />
-                                    </button>
+                                        <div
+                                            v-else
+                                            class="h-8 w-8 bg-indigo-500 rounded-full flex items-center justify-center text-white"
+                                        >
+                                            <!-- TODO: this should not be hardcoded! -->
+                                            KS
+                                        </div>
+                                    </MenuButton>
                                 </div>
-                            </TransitionChild>
-                            <div class="flex flex-shrink-0 items-center px-4">
-                                <img
-                                    class="h-8 w-auto"
-                                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300"
-                                    alt="Your Company"
-                                />
-                            </div>
-                            <div class="mt-5 h-0 flex-1 overflow-y-auto">
-                                <nav class="space-y-1 px-2">
-                                    <NuxtLink
-                                        v-for="item in navigation"
-                                        :key="item.name"
-                                        :to="item.href"
-                                        :class="[
-                                            item.to ==
-                                            router.currentRoute.value.path
-                                                ? 'bg-indigo-800 text-white'
-                                                : 'text-indigo-100 hover:bg-indigo-600',
-                                            'group flex items-center px-2 py-2 text-base font-medium rounded-md',
-                                        ]"
+                                <transition
+                                    enter-active-class="transition ease-out duration-100"
+                                    enter-from-class="transform opacity-0 scale-95"
+                                    enter-to-class="transform opacity-100 scale-100"
+                                    leave-active-class="transition ease-in duration-75"
+                                    leave-from-class="transform opacity-100 scale-100"
+                                    leave-to-class="transform opacity-0 scale-95"
+                                >
+                                    <MenuItems
+                                        class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                                     >
-                                        <component
-                                            :is="item.icon"
-                                            class="mr-4 h-6 w-6 flex-shrink-0 text-indigo-300"
-                                            aria-hidden="true"
-                                        />
-                                        {{ item.name }}
-                                    </NuxtLink>
-                                </nav>
-                            </div>
-                        </DialogPanel>
-                    </TransitionChild>
-                    <div class="w-14 flex-shrink-0" aria-hidden="true">
-                        <!-- Dummy element to force sidebar to shrink to fit close icon -->
+                                        <MenuItem
+                                            v-for="item in userNavigation"
+                                            :key="item.name"
+                                            v-slot="{ active }"
+                                        >
+                                            <NuxtLink
+                                                :to="item.to"
+                                                :class="[
+                                                    active ? 'bg-gray-100' : '',
+                                                    'block px-4 py-2 text-sm text-gray-700',
+                                                ]"
+                                                >{{ item.name }}</NuxtLink
+                                            >
+                                        </MenuItem>
+                                        <MenuItem>
+                                            <button
+                                                @click="handleSignout"
+                                                class="w-full text-left block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                            >
+                                                Sign out
+                                            </button>
+                                        </MenuItem>
+                                    </MenuItems>
+                                </transition>
+                            </Menu>
+                        </div>
                     </div>
-                </div>
-            </Dialog>
-        </TransitionRoot>
-
-        <!-- Static sidebar for desktop -->
-        <div class="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
-            <!-- Sidebar component, swap this element with another sidebar if you like -->
-            <div
-                class="flex flex-grow flex-col overflow-y-auto bg-indigo-700 pt-5"
-            >
-                <div class="flex flex-shrink-0 items-center px-4">
-                    <img
-                        class="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=300"
-                        alt="Your Company"
-                    />
-                </div>
-                <div class="mt-5 flex flex-1 flex-col">
-                    <nav class="flex-1 space-y-1 px-2 pb-4">
-                        <NuxtLink
-                            v-for="item in navigation"
-                            :key="item.name"
-                            :to="item.to"
-                            :class="[
-                                item.to == router.currentRoute.value.path
-                                    ? 'bg-indigo-800 text-white'
-                                    : 'text-indigo-100 hover:bg-indigo-600',
-                                'group flex items-center px-2 py-2 text-sm font-medium rounded-md',
-                            ]"
-                        >
-                            <component
-                                :is="item.icon"
-                                class="mr-3 h-6 w-6 flex-shrink-0 text-indigo-300"
-                                aria-hidden="true"
-                            />
-                            {{ item.name }}
-                        </NuxtLink>
-                    </nav>
-                </div>
-            </div>
+                </nav>
+            </header>
         </div>
 
-        <!-- Navbar -->
-        <div class="flex flex-1 flex-col md:pl-64">
-            <div
-                class="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white shadow"
-            >
-                <button
-                    type="button"
-                    class="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
-                    @click="sidebarOpen = true"
-                >
-                    <span class="sr-only">Open sidebar</span>
-                    <Bars3BottomLeftIcon class="h-6 w-6" aria-hidden="true" />
-                </button>
-                <div class="flex flex-1 justify-between px-4">
-                    <div class="flex flex-1"></div>
-                    <div class="ml-4 flex items-center md:ml-6">
-                        <button
-                            type="button"
-                            class="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        >
-                            <span class="sr-only">View notifications</span>
-                            <BellIcon class="h-6 w-6" aria-hidden="true" />
-                        </button>
-
-                        <!-- Profile dropdown -->
-                        <Menu v-if="profile" as="div" class="relative ml-3">
-                            <div>
-                                <MenuButton
-                                    class="flex max-w-xs items-center rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                >
-                                    <span class="sr-only">Open user menu</span>
-                                    <SupabaseImage
-                                        v-if="profile.avatar_url"
-                                        id="avatars"
-                                        :path="profile.avatar_url"
-                                        class="w-8 h-8 rounded-full overflow-hidden"
-                                    />
-                                    <div
-                                        v-else
-                                        class="h-8 w-8 bg-indigo-500 rounded-full flex items-center justify-center text-white"
-                                    >
-                                        <!-- TODO: this should not be hardcoded! -->
-                                        KS
-                                    </div>
-                                </MenuButton>
-                            </div>
-                            <transition
-                                enter-active-class="transition ease-out duration-100"
-                                enter-from-class="transform opacity-0 scale-95"
-                                enter-to-class="transform opacity-100 scale-100"
-                                leave-active-class="transition ease-in duration-75"
-                                leave-from-class="transform opacity-100 scale-100"
-                                leave-to-class="transform opacity-0 scale-95"
-                            >
-                                <MenuItems
-                                    class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                >
-                                    <MenuItem
-                                        v-for="item in userNavigation"
-                                        :key="item.name"
-                                        v-slot="{ active }"
-                                    >
-                                        <NuxtLink
-                                            :to="item.to"
-                                            :class="[
-                                                active ? 'bg-gray-100' : '',
-                                                'block px-4 py-2 text-sm text-gray-700',
-                                            ]"
-                                            >{{ item.name }}</NuxtLink
-                                        >
-                                    </MenuItem>
-                                    <MenuItem>
-                                        <button
-                                            @click="handleSignout"
-                                            class="w-full text-left block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                                        >
-                                            Sign out
-                                        </button>
-                                    </MenuItem>
-                                </MenuItems>
-                            </transition>
-                        </Menu>
-                    </div>
-                </div>
+        <main>
+            <div class="py-6 mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
+                <slot />
             </div>
-
-            <main>
-                <div class="py-6 mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-                    <slot />
-                </div>
-            </main>
-        </div>
+        </main>
     </div>
 </template>
-
-<style>
-    html {
-        height: 100%;
-        background-color: rgb(243 244 246);
-    }
-
-    body {
-        height: 100%;
-    }
-</style>
