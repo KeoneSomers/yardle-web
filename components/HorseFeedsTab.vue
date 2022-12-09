b976ce3156217d59dac3118c23afc17db9654f2e
KeoneSomers <keone.somers@outlook.com>
2022-12-09 20:08:55 +0000

    meds and feeds tabs done

--- components/HorseFeedsTab.vue
+++ components/HorseFeedsTab.vue
@@ -1,12 +1,38 @@
 <script setup>
-    const people = [
-        {
-            name: "Lindsay Walton",
-            title: "Front-end Developer",
-            email: "lindsay.walton@example.com",
-            role: "Member",
-        },
-    ];
+    // imports
+    import CreateRugModal from "@/components/modals/CreateRugModal.vue";
+    import DeleteFeedModal from "@/components/modals/DeleteFeedModal.vue";
+
+    // modal toggles
+    const createModalOpen = ref(false);
+    const deleteModalOpen = ref(false);
+
+    // supabase
+    const client = useSupabaseClient();
+
+    // states
+    const feeds = useState("feeds");
+    const selectedHorseId = useState("selectedHorseId");
+
+    // refs
+    const feedToDelete = ref(0);
+
+    // start - can this be simplified?
+    const { data: _feeds } = await useAsyncData("feeds", async () => {
+        const { data } = await client
+            .from("feeds")
+            .select()
+            .eq("horse_id", selectedHorseId.value);
+        return data;
+    });
+    feeds.value = _feeds.value;
+    // end
+
+    // functions
+    const handleDelete = (feedId) => {
+        feedToDelete.value = feedId;
+        deleteModalOpen.value = true;
+    };
 </script>
 
 <template>
@@ -15,16 +41,17 @@
             <div class="sm:flex-auto">
                 <h1 class="text-xl font-semibold text-gray-900">Feeds</h1>
                 <p class="mt-2 text-sm text-gray-700">
-                    A list of all the rugs that belong to this horse including
+                    A list of all the feeds that belong to this horse including
                     their description, type, weight and age.
                 </p>
             </div>
             <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
                 <button
+                    @click="() => (createModalOpen = true)"
                     type="button"
                     class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
                 >
-                    Add rug
+                    Add feed
                 </button>
             </div>
         </div>
@@ -51,47 +78,50 @@
                                         scope="col"
                                         class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
                                     >
-                                        Title
+                                        Ingredients
                                     </th>
                                     <th
                                         scope="col"
                                         class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
                                     >
-                                        Email
+                                        Instructions
                                     </th>
                                     <th
                                         scope="col"
                                         class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-6"
-                                    >
-                                        Role
-                                    </th>
+                                    ></th>
                                 </tr>
                             </thead>
                             <tbody class="divide-y divide-gray-200 bg-white">
                                 <tr
-                                    v-for="person in people"
-                                    :key="person.email"
+                                    v-for="feed in feeds"
+                                    :key="feed.id"
                                     class="divide-x divide-gray-200 grid grid-cols-4"
                                 >
                                     <td
                                         class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-6 break-all"
                                     >
-                                        {{ person.name }}
+                                        {{ feed.name }}
                                     </td>
                                     <td
                                         class="p-4 text-sm text-gray-500 break-all break-all"
                                     >
-                                        {{ person.title }}
+                                        {{ feed.ingredients }}
                                     </td>
                                     <td
                                         class="p-4 text-sm text-gray-500 break-all"
                                     >
-                                        {{ person.email }}
+                                        {{ feed.instructions }}
                                     </td>
                                     <td
                                         class="py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-6 break-all"
                                     >
-                                        {{ person.role }}
+                                        <button
+                                            @click="handleDelete(feed.id)"
+                                            class="bg-red-400 rounded px-3 py-1 text-white"
+                                        >
+                                            Delete
+                                        </button>
                                     </td>
                                 </tr>
                             </tbody>
@@ -101,4 +131,15 @@
             </div>
         </div>
     </div>
+
+    <!-- Modals -->
+    <CreateRugModal
+        :is-open="createModalOpen"
+        @close="createModalOpen = false"
+    />
+    <DeleteFeedModal
+        :is-open="deleteModalOpen"
+        :feed-id="feedToDelete"
+        @close="deleteModalOpen = false"
+    />
 </template>
--- components/HorseMedicationsTab.vue
+++ components/HorseMedicationsTab.vue
@@ -1,12 +1,41 @@
 <script setup>
-    const people = [
-        {
-            name: "Lindsay Walton",
-            title: "Front-end Developer",
-            email: "lindsay.walton@example.comlindsay.walton@example.comlindsay.walton@example.comlindsay.walton@example.comlindsay.walton@example.comlindsay.walton@example.com",
-            role: "Member",
-        },
-    ];
+    // imports
+    import CreateRugModal from "@/components/modals/CreateRugModal.vue";
+    import DeleteMedicationModal from "@/components/modals/DeleteMedicationModal.vue";
+
+    // modal toggles
+    const createModalOpen = ref(false);
+    const deleteModalOpen = ref(false);
+
+    // supabase
+    const client = useSupabaseClient();
+
+    // states
+    const medications = useState("medications");
+    const selectedHorseId = useState("selectedHorseId");
+
+    // refs
+    const medicationToDelete = ref(0);
+
+    // start - can this be simplified?
+    const { data: _medications } = await useAsyncData(
+        "medications",
+        async () => {
+            const { data } = await client
+                .from("medications")
+                .select()
+                .eq("horse_id", selectedHorseId.value);
+            return data;
+        }
+    );
+    medications.value = _medications.value;
+    // end
+
+    // functions
+    const handleDelete = (medicationId) => {
+        medicationToDelete.value = medicationId;
+        deleteModalOpen.value = true;
+    };
 </script>
 
 <template>
@@ -15,23 +44,24 @@
             <div class="sm:flex-auto">
                 <h1 class="text-xl font-semibold text-gray-900">Medications</h1>
                 <p class="mt-2 text-sm text-gray-700">
-                    A list of all the rugs that belong to this horse including
-                    their description, type, weight and age.
+                    A list of all the medications that belong to this horse
+                    including their description, type, weight and age.
                 </p>
             </div>
             <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
                 <button
+                    @click="() => (createModalOpen = true)"
                     type="button"
                     class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
                 >
-                    Add rug
+                    Add medication
                 </button>
             </div>
         </div>
         <div class="mt-8 flex flex-col">
             <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div
-                    class="inline-block w-full py-2 align-middle md:px-6 lg:px-8"
+                    class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8"
                 >
                     <div
                         class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg"
@@ -51,47 +81,39 @@
                                         scope="col"
                                         class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
                                     >
-                                        Title
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
-                                    >
-                                        Email
+                                        Instructions
                                     </th>
                                     <th
                                         scope="col"
                                         class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-6"
-                                    >
-                                        Role
-                                    </th>
+                                    ></th>
                                 </tr>
                             </thead>
                             <tbody class="divide-y divide-gray-200 bg-white">
                                 <tr
-                                    v-for="person in people"
-                                    :key="person.email"
+                                    v-for="medication in medications"
+                                    :key="medication.id"
                                     class="divide-x divide-gray-200 grid grid-cols-4"
                                 >
                                     <td
                                         class="py-4 pl-4 pr-4 text-sm font-medium text-gray-900 sm:pl-6 break-all"
                                     >
-                                        {{ person.name }}
+                                        {{ medication.name }}
                                     </td>
                                     <td
                                         class="p-4 text-sm text-gray-500 break-all break-all"
                                     >
-                                        {{ person.title }}
-                                    </td>
-                                    <td
-                                        class="p-4 text-sm text-gray-500 break-all"
-                                    >
-                                        {{ person.email }}
+                                        {{ medication.instructions }}
                                     </td>
                                     <td
                                         class="py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-6 break-all"
                                     >
-                                        {{ person.role }}
+                                        <button
+                                            @click="handleDelete(medication.id)"
+                                            class="bg-red-400 rounded px-3 py-1 text-white"
+                                        >
+                                            Delete
+                                        </button>
                                     </td>
                                 </tr>
                             </tbody>
@@ -101,4 +123,15 @@
             </div>
         </div>
     </div>
+
+    <!-- Modals -->
+    <CreateRugModal
+        :is-open="createModalOpen"
+        @close="createModalOpen = false"
+    />
+    <DeleteMedicationModal
+        :is-open="deleteModalOpen"
+        :medication-id="medicationToDelete"
+        @close="deleteModalOpen = false"
+    />
 </template>
