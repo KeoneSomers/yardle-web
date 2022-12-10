6103f3e0c2b5a9a735350908cf82d406896626da
KeoneSomers <keone.somers@outlook.com>
2022-12-10 13:25:46 +0000

    middleware work

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,31 +1,24 @@
 export default defineNuxtRouteMiddleware((to) => {
     const user = useSupabaseUser();
 
+    // variables
     const isLoggedIn = user.value != null;
     const isLoggedOut = user.value == null;
     const hasSelectedYard =
         user.value != null && user.value.user_metadata.selected_yard != null;
 
-    // SCENARIO: Trying to access a protected page - must be logged in
-    // if (
-    //     to.path != "/" &&
-    //     to.path != "/login" &&
-    //     to.path != "/signup" &&
-    //     isLoggedOut
-    // ) {
-    //     return navigateTo("/");
-    // }
-    // } else if (
-    //     // SCENARIO: Trying to access login or signup page - must be logged out
-    //     (to.path == "/" || to.path == "/login" || to.path == "/signup") &&
-    //     isLoggedIn
-    // ) {
-    //     // todo - also check token has not expired! (currently set to 1 week)
-    //     return navigateTo("/horses");
-    // }
+    const nonProtectedPages = ["/", "/login", "signup"];
 
-    // SCENARIO: User is logged in but has not selected a yard
-    // if (!hasSelectedYard && to.path != "/yards") {
-    //     return navigateTo("/yards");
-    // }
+    // visiter is trying to access non auth pages
+    if (nonProtectedPages.includes(to.path)) {
+        if (isLoggedIn) {
+            if (hasSelectedYard) {
+                navigateTo("/horses");
+            } else {
+                navigateTo("/yards");
+            }
+        }
+    }
+
+    // TODO: visiter is trying to access auth pages
 });
