e85c22f8488e0671f5e0a4cbca2d1ffa2370657a
KeoneSomers <keone.somers@outlook.com>
2022-12-10 15:28:57 +0000

    middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -9,7 +9,7 @@ export default defineNuxtRouteMiddleware((to) => {
 
     const nonProtectedPages = ["/", "/login", "signup"];
 
-    // visiter is trying to access non auth pages
+    // visiter is trying to access NON-AUTH pages
     if (nonProtectedPages.includes(to.path)) {
         if (isLoggedIn) {
             if (hasSelectedYard) {
@@ -20,5 +20,27 @@ export default defineNuxtRouteMiddleware((to) => {
         }
     }
 
-    // TODO: visiter is trying to access auth pages
+    // visiter is trying to access AUTH pages
+    if (!nonProtectedPages.includes(to.path)) {
+        if (isLoggedOut) navigateTo("/");
+
+        if (isLoggedIn) {
+            // already have selected yard
+            if (hasSelectedYard) {
+                if (to.path == "/yards") navigateTo("/horses");
+            }
+
+            // have not selected a yard
+            if (!hasSelectedYard) {
+                // TODO: use array include to get this down to 1 line
+                if (to.path == "/dashboard") navigateTo("/yards");
+                if (to.path == "/horses") navigateTo("/yards");
+                if (to.path == "/members") navigateTo("/yards");
+                if (to.path == "/calendar") navigateTo("/yards");
+                if (to.path == "/fields") navigateTo("/yards");
+                if (to.path == "/report/rugs") navigateTo("/yards");
+                if (to.path == "/report/feeds") navigateTo("/yards");
+            }
+        }
+    }
 });
