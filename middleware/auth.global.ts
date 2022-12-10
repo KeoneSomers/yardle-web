94b21d36704ac34d38b6ba951cb4b9e97999b824
KeoneSomers <keone.somers@outlook.com>
2022-12-10 16:39:26 +0000

    cleaned up middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -32,14 +32,17 @@ export default defineNuxtRouteMiddleware((to) => {
 
             // have not selected a yard
             if (!hasSelectedYard) {
-                // TODO: use array include to get this down to 1 line
-                if (to.path == "/dashboard") navigateTo("/yards");
-                if (to.path == "/horses") navigateTo("/yards");
-                if (to.path == "/members") navigateTo("/yards");
-                if (to.path == "/calendar") navigateTo("/yards");
-                if (to.path == "/fields") navigateTo("/yards");
-                if (to.path == "/report/rugs") navigateTo("/yards");
-                if (to.path == "/report/feeds") navigateTo("/yards");
+                const yardProtectedPages = [
+                    "/dashboard",
+                    "/horses",
+                    "/members",
+                    "/calendar",
+                    "/fields",
+                    "/report/rugs",
+                    "/report/feeds",
+                ];
+
+                if (yardProtectedPages.includes(to.path)) navigateTo("/yards");
             }
         }
     }
