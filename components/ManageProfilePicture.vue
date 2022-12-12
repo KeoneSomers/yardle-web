3ccc0eb72cd2a26e62f9f6174fbbc980bc2b5a59
KeoneSomers <keone.somers@outlook.com>
2022-12-12 16:07:55 +0000

    minor style changes

--- components/ManageProfilePicture.vue
+++ components/ManageProfilePicture.vue
@@ -57,7 +57,7 @@
 </script>
 
 <template>
-    <div class="m-4 border p-4 inline-block bg-gray-100">
+    <!-- <div class="m-4 border p-4 inline-block bg-gray-100">
         <img v-if="src" :src="src" alt="Avatar" class="avatar image w-5 h-5" />
 
         <div style="width: 10em; position: relative">
@@ -71,6 +71,50 @@
                 @change="uploadAvatar"
                 :disabled="uploading"
             />
+        </div>
+    </div> -->
+    <div>
+        <label for="photo" class="block text-sm font-medium text-gray-700"
+            >Photo</label
+        >
+        <div class="mt-1 sm:col-span-2 sm:mt-0">
+            <div class="flex items-center">
+                <span
+                    class="h-12 w-12 overflow-hidden rounded-full bg-gray-100 mr-4 mt-2"
+                >
+                    <div class="w-26 h-26">
+                        <img
+                            v-if="src"
+                            :src="src"
+                            alt="Avatar"
+                            class="object-cover"
+                        />
+                        <svg
+                            v-else
+                            class="h-full w-full text-gray-300"
+                            fill="currentColor"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"
+                            />
+                        </svg>
+                    </div>
+                </span>
+                <!-- <button
+                    type="button"
+                    class="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                    Change
+                </button> -->
+                <input
+                    type="file"
+                    id="single"
+                    accept="image/*"
+                    @change="uploadAvatar"
+                    :disabled="uploading"
+                />
+            </div>
         </div>
     </div>
 </template>
