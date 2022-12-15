a12f0e73a8dde8b7720cef9912fb93175b36b6f9
KeoneSomers <keone.somers@outlook.com>
2022-12-15 12:26:32 +0000

    getting basic invite yard details

--- pages/join/[invite_code].vue
+++ pages/join/[invite_code].vue
@@ -5,10 +5,48 @@
 
     const { invite_code } = useRoute().params;
     const user = useSupabaseUser();
+    const yard = ref(null);
+    const client = useSupabaseClient();
+
+    // first get some basic info about the yard from the db
+    const getBasicYardData = async () => {
+        await useAsyncData("yardBasic", async () => {
+            const { data, error } = await client
+                .from("yards")
+                .select("id, name")
+                .eq("invite_code", invite_code)
+                .single();
+
+            yard.value = data;
+        });
+    };
+
+    onMounted(async () => {
+        await getBasicYardData();
+
+        watchEffect(async () => {
+            if (invite_code && yard.value == null) {
+                await getBasicYardData();
+            }
+        });
+    });
+
+    // TODO: if user is already a member of this yard, redirect them to the /horses page
 </script>
 
 <template>
-    <div>{{ invite_code }}</div>
-    <div v-if="user">You're logged in!</div>
-    <div v-else>You need to create an account first!</div>
+    <div class="flex items-center justify-center h-screen text-center">
+        <div v-if="yard" class="border p-5 rounded-lg m-5">
+            <div class="text-2xl mb-3">
+                You've been invited to join<br /><span
+                    class="text-indigo-400"
+                    >{{ yard.name }}</span
+                >
+            </div>
+            <div v-if="user">You're logged in!</div>
+            <div v-else>
+                You'll need to create an account or login before you can join.
+            </div>
+        </div>
+    </div>
 </template>
