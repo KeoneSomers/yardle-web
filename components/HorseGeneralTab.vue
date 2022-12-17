8c074f1962f611746d520d3c0e0c510ec2e0c814
KeoneSomers <keone.somers@outlook.com>
2022-12-17 15:45:29 +0000

    refactored horse gen tab

--- components/HorseGeneralTab.vue
+++ components/HorseGeneralTab.vue
@@ -2,84 +2,103 @@
     import { DateTime } from "luxon";
     const horse = useState("horse");
 
-    const getAge = (dateString) => {
-        var today = new Date();
-        var birthDate = new Date(dateString);
-        var age = today.getFullYear() - birthDate.getFullYear();
-        var m = today.getMonth() - birthDate.getMonth();
-        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-            age--;
-        }
-        return age;
-    };
-
-    const fields = ref([
-        {
-            title: "Last Farrier visit",
-            value: "--",
-        },
-        {
-            title: "Last Dentist visit",
-            value: "--",
-        },
-        {
-            title: "Last Worming",
-            value: "--",
-        },
-        {
-            title: "Last Vaccination",
-            value: "--",
-        },
-        {
-            title: "Date of Birth",
-            value: "--",
-        },
-        {
-            title: "Breed",
-            value: "",
-        },
-        {
-            title: "Color & Markings",
-            value: horse.value.color_markings,
-        },
-    ]);
-
-    watchEffect(() => {
-        if (horse.value.dob) {
-            fields.value[4].value = DateTime.fromISO(
-                String(horse.value.dob)
-            ).toLocaleString(DateTime.DATE_MED);
-        } else {
-            fields.value[4].value = "--";
-        }
-
-        if (horse.value.breed) {
-            fields.value[5].value = horse.value.breed;
-        } else {
-            fields.value[5].value = "--";
-        }
-
-        if (horse.value.color_markings) {
-            fields.value[6].value = horse.value.color_markings;
-        } else {
-            fields.value[6].value = "--";
-        }
-    });
+    // const getAge = (dateString) => {
+    //     var today = new Date();
+    //     var birthDate = new Date(dateString);
+    //     var age = today.getFullYear() - birthDate.getFullYear();
+    //     var m = today.getMonth() - birthDate.getMonth();
+    //     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    //         age--;
+    //     }
+    //     return age;
+    // };
 </script>
 
 <template>
     <div class="mx-auto my-6 max-w-5xl px-4 sm:px-6 lg:px-8">
         <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
-            <div
-                v-for="field in fields"
-                :key="field.title"
-                class="sm:col-span-1"
-            >
+            <!-- Farrier -->
+            <div class="sm:col-span-1">
                 <dt class="text-sm font-medium text-gray-500">
-                    {{ field.title }}
+                    Last Farrier Visit
                 </dt>
+                <dd class="mt-1 text-sm text-gray-900">--</dd>
+            </div>
+            <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">
+                    Next Farrier Visit
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900">--</dd>
+            </div>
+
+            <!-- Dentist -->
+            <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">
+                    Last Dentist Visit
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900">--</dd>
+            </div>
+            <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">
+                    Next Dentist Visit
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900">--</dd>
+            </div>
+
+            <!-- Worming -->
+            <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">Last Worming</dt>
+                <dd class="mt-1 text-sm text-gray-900">--</dd>
+            </div>
+            <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">Next Worming</dt>
+                <dd class="mt-1 text-sm text-gray-900">--</dd>
+            </div>
+
+            <!-- Vaccinations -->
+            <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">
+                    Last Vaccinations
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900">--</dd>
+            </div>
+            <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">
+                    Next Vaccinations
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900">--</dd>
+            </div>
+
+            <!-- Breed -->
+            <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">Date of Birth</dt>
                 <dd class="mt-1 text-sm text-gray-900">
-                    {{ field.value }}
+                    <span v-if="horse.breed">{{ horse.breed }}</span>
+                    <span v-else>--</span>
+                </dd>
+            </div>
+
+            <!-- Color & Markings -->
+            <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">Date of Birth</dt>
+                <dd class="mt-1 text-sm text-gray-900">
+                    <span v-if="horse.color_markings">{{
+                        horse.color_markings
+                    }}</span>
+                    <span v-else>--</span>
+                </dd>
+            </div>
+
+            <!-- DoB -->
+            <div class="sm:col-span-1">
+                <dt class="text-sm font-medium text-gray-500">Date of Birth</dt>
+                <dd class="mt-1 text-sm text-gray-900">
+                    <span v-if="horse.dob">{{
+                        DateTime.fromISO(String(horse.dob)).toLocaleString(
+                            DateTime.DATE_MED
+                        )
+                    }}</span>
+                    <span v-else>--</span>
                 </dd>
             </div>
 
