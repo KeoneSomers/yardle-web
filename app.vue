1bd19b00a4c2af8ce4a786b259edb7eb27bca69e
KeoneSomers <keone.somers@outlook.com>
2022-12-21 11:41:38 +0000

    package updates

--- app.vue
+++ app.vue
@@ -1,9 +1,17 @@
-<script setup>
+<script setup lang="ts">
     const user = useSupabaseUser();
+    const authClient = useSupabaseAuthClient();
+
     const _user = useState("user", () => user.value);
 
     watchEffect(() => {
         _user.value = user.value;
+    });
+
+    authClient.auth.onAuthStateChange((event, session) => {
+        if (event === "TOKEN_REFRESHED") {
+            // BUG: token is not being refreshed on client and therefore requests stop working.
+        }
     });
 </script>
 
