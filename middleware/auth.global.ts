b41d22ed92c37d1925248aef74790c67119b0b06
KeoneSomers <keone.somers@outlook.com>
2023-01-11 00:34:44 +0000

    test deployment for middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -4,17 +4,17 @@ export default defineNuxtRouteMiddleware(async (to) => {
     const selectedYard = useState("selectedYard");
 
     // on cold page load - get the users selected yard directly from the db since it will be undefined in the store at this point.
-    if (user.value && !selectedYard.value) {
-        const { data } = await client
-            .from("profiles")
-            .select("selected_yard")
-            .eq("id", user.value?.id)
-            .single();
+    // if (user.value && !selectedYard.value) {
+    //     const { data } = await client
+    //         .from("profiles")
+    //         .select("selected_yard")
+    //         .eq("id", user.value?.id)
+    //         .single();
 
-        if (data) {
-            selectedYard.value = data.selected_yard;
-        }
-    }
+    //     if (data) {
+    //         selectedYard.value = data.selected_yard;
+    //     }
+    // }
 
     // TODO: middleware overhaul. split-out into multiple files and then define each middleware on each page
 
