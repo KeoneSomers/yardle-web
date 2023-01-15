5434d6b8667af3a9529503a3dec6d0cc8c1c4fef
KeoneSomers <keone.somers@outlook.com>
2023-01-15 13:47:31 +0000

    hidden the profile link

--- components/DesktopSidebar.vue
+++ components/DesktopSidebar.vue
@@ -235,13 +235,13 @@
                                         <MenuItems
                                             class="absolute bottom-0 right-0 z-10 mt-2 w-48 origin-bottom-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                                         >
-                                            <MenuItem>
+                                            <!-- <MenuItem>
                                                 <NuxtLink
                                                     to="/profile"
                                                     class="block px-4 py-2 text-sm text-gray-700"
                                                     >Your Profile</NuxtLink
                                                 >
-                                            </MenuItem>
+                                            </MenuItem> -->
                                             <MenuItem>
                                                 <NuxtLink
                                                     to="/settings"
