af67d683a0bdca7c200d8a23e30ef7cf222c462d
KeoneSomers <keone.somers@outlook.com>
2023-01-18 11:22:14 +0000

    delete feed from report page

--- pages/report/feeds.vue
+++ pages/report/feeds.vue
@@ -1,10 +1,17 @@
 <script setup>
+import BasicModal from "@/components/BasicModal.vue";
+import { DialogTitle } from "@headlessui/vue";
+import { ExclamationTriangleIcon } from "@heroicons/vue/24/outline/index.js";
+
 definePageMeta({
   guards: ["requireAuth", "requireYard"],
 });
 
 const client = useSupabaseClient();
 const selectedYard = useState("selectedYard");
+
+const deleteModalOpen = ref(false);
+const selectedFeedId = ref(0);
 
 const ingredientTypes = [
   "Pick one",
@@ -59,6 +66,47 @@ const { data: feeds } = await useAsyncData("all_feeds", async () => {
 
   return Promise.all(mappedFeeds);
 });
+
+const handleDelete = async () => {
+  // delete the feeds ingredients
+  const { error: ingredientsError } = await client
+    .from("ingredients")
+    .delete()
+    .eq("feed_id", selectedFeedId.value)
+    .select();
+
+  if (ingredientsError) {
+    // somthing went wrong!
+    console.log(ingredientsError);
+    return;
+  }
+
+  // delete the feed
+  const { error: feedError } = await client
+    .from("feeds")
+    .delete()
+    .eq("id", selectedFeedId.value)
+    .select();
+
+  if (feedError) {
+    // somthing went wrong!
+    console.log(feedError);
+    return;
+  }
+
+  // success! - now remove the deleted feed from the webpage
+  const index = feeds.value.map((e) => e.id).indexOf(selectedFeedId.value);
+  feeds.value.splice(index, 1);
+
+  // close the modal
+  deleteModalOpen.value = false;
+  selectedFeedId.value = 0;
+};
+
+const handleModalOpen = (feedId) => {
+  deleteModalOpen.value = true;
+  selectedFeedId.value = feedId;
+};
 </script>
 
 <template>
@@ -92,9 +140,7 @@ const { data: feeds } = await useAsyncData("all_feeds", async () => {
                   <th
                     scope="col"
                     class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                  >
-                    Horse
-                  </th>
+                  ></th>
                   <th
                     scope="col"
                     class="px-4 py-3.5 text-left text-sm font-semibold text-gray-900"
@@ -125,6 +171,10 @@ const { data: feeds } = await useAsyncData("all_feeds", async () => {
                   >
                     Other
                   </th>
+                  <th
+                    scope="col"
+                    class="py-3.5 pl-4 pr-4 text-left text-sm font-semibold text-gray-900 sm:pr-6"
+                  ></th>
                 </tr>
               </thead>
               <tbody class="divide-y divide-gray-200 bg-white">
@@ -264,6 +314,16 @@ const { data: feeds } = await useAsyncData("all_feeds", async () => {
                       </span>
                     </div>
                   </td>
+                  <td
+                    class="whitespace-nowrap py-4 pl-4 pr-4 text-sm text-gray-500 sm:pr-6"
+                  >
+                    <button
+                      @click="handleModalOpen(feed.id)"
+                      class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+                    >
+                      Delete
+                    </button>
+                  </td>
                 </tr>
               </tbody>
             </table>
@@ -272,4 +332,51 @@ const { data: feeds } = await useAsyncData("all_feeds", async () => {
       </div>
     </div>
   </div>
+
+  <!-- Modals -->
+  <BasicModal :is-open="deleteModalOpen" @close="deleteModalOpen = false">
+    <div>
+      <div class="sm:flex sm:items-start">
+        <div
+          class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
+        >
+          <ExclamationTriangleIcon
+            class="h-6 w-6 text-red-600"
+            aria-hidden="true"
+          />
+        </div>
+        <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+          <DialogTitle
+            as="h3"
+            class="text-lg font-medium leading-6 text-gray-900"
+            >Delete Feed</DialogTitle
+          >
+          <div class="mt-2">
+            <p class="text-sm text-gray-500">
+              Are you sure you want to delete this feed? All of it's data will
+              be permanently removed from your yard forever. This action cannot
+              be undone.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+        <button
+          type="button"
+          class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+          @click="handleDelete"
+        >
+          Delete
+        </button>
+        <button
+          type="button"
+          class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:w-auto sm:text-sm"
+          @click="deleteModalOpen = false"
+          ref="cancelButtonRef"
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  </BasicModal>
 </template>
