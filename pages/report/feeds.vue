cf4dd6e3b820df6f407f5442224a0d73e79828d6
KeoneSomers <keone.somers@outlook.com>
2023-01-28 09:34:14 +0000

    bug fix for ingredient qtys

--- pages/report/feeds.vue
+++ pages/report/feeds.vue
@@ -162,8 +162,8 @@ const setShadow = (event) => {
 </script>
 
 <template>
-  <div class="px-4 sm:px-6 lg:px-8 my-10">
-    <div class="sm:flex sm:items-center">
+  <div class="px-4 sm:px-6 lg:px-8 h-full flex flex-col">
+    <div class="sm:flex sm:items-center my-16">
       <div class="sm:flex-auto">
         <h1 class="text-xl font-semibold text-gray-900">Feed Report</h1>
         <p class="mt-2 text-sm text-gray-700">
@@ -171,7 +171,7 @@ const setShadow = (event) => {
           ingredients, instructions and conditions.
         </p>
       </div>
-      <div class="mt-4 sm:mt-0 sm:ml-16 flex">
+      <div v-show="feeds.length > 0" class="mt-4 sm:mt-0 sm:ml-16 flex">
         <button
           @click="() => (createModalOpen = true)"
           type="button"
@@ -184,7 +184,7 @@ const setShadow = (event) => {
       </div>
     </div>
 
-    <div class="relative">
+    <div v-show="feeds.length > 0" class="relative flex-1 overflow-y-auto">
       <div
         ref="edgeShadowLeft"
         v-if="edgeShadowLeftShow"
@@ -365,6 +365,42 @@ const setShadow = (event) => {
             </tr>
           </tbody>
         </table>
+      </div>
+    </div>
+    <div
+      v-show="feeds.length == 0"
+      class="flex w-full flex-1 justify-center items-center"
+    >
+      <div class="text-center">
+        <svg
+          class="mx-auto h-12 w-12 text-gray-400"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            vector-effect="non-scaling-stroke"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+          />
+        </svg>
+        <h3 class="mt-2 text-sm font-medium text-gray-900">No Feeds</h3>
+        <p class="mt-1 text-sm text-gray-500">
+          Feeds that belong to your horses will be shown here.
+        </p>
+        <div class="mt-6">
+          <button
+            @click="() => (createModalOpen = true)"
+            type="button"
+            class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            <PlusIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+            New Feed
+          </button>
+        </div>
       </div>
     </div>
   </div>
