d86c69a569116d3567d84a7b668d020d490fe192
KeoneSomers <keone.somers@outlook.com>
2023-01-31 13:07:28 +0000

    copyright text change

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="flex justify-center p-5 border-t">
-    &copy; Copyright Keone Somers. All Rights Reserved. Copyright Protected with
-    www.ProtectMyWork.com, Reference Number: 19584250123S049 <br />
-    Get in touch: Keone.somers@outlook.com
+    &copy; Copyright. All Rights Reserved. Get in touch:
+    Keone.somers@outlook.com
   </div>
 </template>
--- components/SidebarFooter.vue
+++ components/SidebarFooter.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="flex justify-center p-2 border-t text-xs text-gray-500">
-    &copy; Copyright Keone Somers. All Rights Reserved. Copyright Protected with
-    www.ProtectMyWork.com, Reference Number: 19584250123S049
+    &copy; Copyright. All Rights Reserved.
   </div>
 </template>
