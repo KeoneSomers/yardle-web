0c12d40fdf95115e475c2fbcb987f28f9bf37ae2
KeoneSomers <keone.somers@outlook.com>
2023-02-11 16:31:04 +0000

    patch

--- composables/useRouteManager.ts
+++ composables/useRouteManager.ts
@@ -10,56 +10,57 @@ export function useRouteManager() {
   });
 
   const router = useRouter();
+  const route = useRoute();
   const selectedYard = useState("selectedYard");
   // invite code if there is one (could also be undefined or null)
 
-  // router.afterEach((to) => {
-  //   console.log(to.fullPath);
-  //   sidebarOpen.value = false;
+  watchEffect(async () => {
+    console.log(route.fullPath);
+    sidebarOpen.value = false;
 
-  //   const { invite_code } = to.query;
-  //   const requireAuth = to.meta.guards.includes("requireAuth");
-  //   const requireNoAuth = to.meta.guards.includes("requireNoAuth");
-  //   const requireYard = to.meta.guards.includes("requireYard");
-  //   const requireNoYard = to.meta.guards.includes("requireNoYard");
+    const { invite_code } = route.query;
+    const requireAuth = route.meta.guards.includes("requireAuth");
+    const requireNoAuth = route.meta.guards.includes("requireNoAuth");
+    const requireYard = route.meta.guards.includes("requireYard");
+    const requireNoYard = route.meta.guards.includes("requireNoYard");
 
-  //   // This code first checks if the user is not authenticated, and if they are not and requireAuth is true, it navigates to the homepage and exits the function.
-  //   if (!user.value) {
-  //     if (requireAuth) {
-  //       navigateTo("/");
-  //     }
-  //     return;
-  //   }
+    // This code first checks if the user is not authenticated, and if they are not and requireAuth is true, it navigates to the homepage and exits the function.
+    if (!user.value) {
+      if (requireAuth) {
+        navigateTo("/");
+      }
+      return;
+    }
 
-  //   // Then, it checks the value of requireNoAuth. If it's true, it checks if there's a selectedYard and navigates to the appropriate page.
-  //   if (requireNoAuth) {
-  //     if (invite_code) {
-  //       navigateTo("/join/" + invite_code);
-  //       return;
-  //     }
+    // Then, it checks the value of requireNoAuth. If it's true, it checks if there's a selectedYard and navigates to the appropriate page.
+    if (requireNoAuth) {
+      if (invite_code) {
+        navigateTo("/join/" + invite_code);
+        return;
+      }
 
-  //     if (selectedYard.value) {
-  //       navigateTo("/yard/horses");
-  //     } else {
-  //       navigateTo("/yards");
-  //     }
-  //     return;
-  //   }
+      if (selectedYard.value) {
+        navigateTo("/yard/horses");
+      } else {
+        navigateTo("/yards");
+      }
+      return;
+    }
 
-  //   // Then, it checks the value of requireYard. If it's true, it checks if there's no selectedYard and navigates to the appropriate page.
-  //   if (requireYard) {
-  //     if (!selectedYard.value) {
-  //       navigateTo("/yards");
-  //     }
-  //     return;
-  //   }
+    // Then, it checks the value of requireYard. If it's true, it checks if there's no selectedYard and navigates to the appropriate page.
+    if (requireYard) {
+      if (!selectedYard.value) {
+        navigateTo("/yards");
+      }
+      return;
+    }
 
-  //   // Then, it checks the value of requireNoYard. If it's true, it checks if there's a selectedYard and navigates to the appropriate page.
-  //   if (requireNoYard) {
-  //     if (selectedYard.value) {
-  //       navigateTo("/yard/horses");
-  //     }
-  //     return;
-  //   }
-  // });
+    // Then, it checks the value of requireNoYard. If it's true, it checks if there's a selectedYard and navigates to the appropriate page.
+    if (requireNoYard) {
+      if (selectedYard.value) {
+        navigateTo("/yard/horses");
+      }
+      return;
+    }
+  });
 }
