ab21d5085c5fda15c65ccbf96064d0f37f070a22
KeoneSomers <keone.somers@outlook.com>
2023-02-14 09:54:28 +0000

    removed tawk chat - too slow

--- plugins/tawk-messenger.client.js
+++
@@ -1,8 +0,0 @@
-import TawkMessengerVue from "@tawk.to/tawk-messenger-vue-3";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(TawkMessengerVue, {
-    propertyId: "63ea5213c2f1ac1e203305ab",
-    widgetId: "1gp5l17pf",
-  });
-});
