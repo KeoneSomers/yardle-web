fff54fc1211984a06907a967503d193044517e1b
KeoneSomers <keone.somers@outlook.com>
2023-02-22 19:03:02 +0000

    updated footer

--- components/SidebarFooter.vue
+++ components/SidebarFooter.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="flex justify-center p-2 border-t text-xs text-gray-400">
-    &copy; {{ new Date().getFullYear() }} Copyright. All Rights Reserved. Svg
-    art by SVGBackgrounds.com
+    &copy; {{ new Date().getFullYear() }} Copyright. All Rights Reserved.
   </div>
 </template>
