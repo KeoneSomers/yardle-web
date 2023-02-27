649b6aff7d7c440b17e54b70142048311c589f13
KeoneSomers <keone.somers@outlook.com>
2023-02-27 09:34:08 +0000

    patch

--- pages/yard/invoices.vue
+++ pages/yard/invoices.vue
@@ -19,7 +19,7 @@ invoices.value = invoicesData;
 </script>
 
 <template>
-  <div class="overflow-auto pb-20 px-4 md:px-0">
+  <div v-if="invoices.length > 0" class="overflow-auto pb-20 px-4 md:px-0">
     <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
       <div class="py-5">
         <p class="text-4xl font-bold mt-20">Manage Invoices</p>
@@ -51,6 +51,31 @@ invoices.value = invoicesData;
           </button>
         </div>
       </div>
+    </div>
+  </div>
+  <!-- Empty State -->
+  <div v-else class="flex w-full flex-1 justify-center items-center">
+    <div class="text-center">
+      <svg
+        class="mx-auto h-12 w-12 text-gray-400"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        aria-hidden="true"
+      >
+        <path
+          vector-effect="non-scaling-stroke"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+        />
+      </svg>
+      <h3 class="mt-2 text-sm font-medium text-gray-900">No Invoices</h3>
+      <p class="mt-1 text-sm text-gray-500">
+        Invoices that are ready to be sent out to your clients will be shown
+        here.
+      </p>
     </div>
   </div>
 </template>
