bd66df72d66c32e72037367943a9e592082351e1
KeoneSomers <keone.somers@outlook.com>
2023-03-06 17:31:21 +0000

    minor

--- components/HorseDetails.vue
+++ components/HorseDetails.vue
@@ -180,178 +180,173 @@ const { open: openDeleteHorseModal, close: closeDeleteHorseModal } = useModal({
 
 <template>
   <div
-    class="relative md:block z-0 flex-1 focus:outline-none xl:order-last overflow-hidden"
+    v-if="horse"
+    class="relative md:block z-0 flex-1 focus:outline-none xl:order-last overflow-y-auto h-[calc(100vh - 16rem)] flex flex-col"
     :class="{ hidden: !viewingHorse }"
   >
-    <main v-if="horse" class="flex flex-col overflow-hidden h-screen pb-20">
-      <!-- Breadcrumb -->
-      <nav
-        class="bg-white flex items-start px-4 py-3 sm:px-6 lg:px-8 md:hidden border-b"
-        aria-label="Breadcrumb"
+    <!-- Breadcrumb -->
+    <nav
+      class="bg-white flex items-start px-4 py-3 sm:px-6 lg:px-8 md:hidden border-b"
+      aria-label="Breadcrumb"
+    >
+      <a
+        @click="() => (viewingHorse = false)"
+        class="inline-flex items-center space-x-1 text-sm font-medium text-blue-500"
       >
-        <a
-          @click="() => (viewingHorse = false)"
-          class="inline-flex items-center space-x-1 text-sm font-medium text-blue-500"
-        >
-          <ChevronLeftIcon class="-ml-2 h-7 w-7" aria-hidden="true" />
-          <span>Horses</span>
-        </a>
-      </nav>
+        <ChevronLeftIcon class="-ml-2 h-7 w-7" aria-hidden="true" />
+        <span>Horses</span>
+      </a>
+    </nav>
 
-      <div ref="horseDetailsElement" class="overflow-y-auto">
-        <article>
-          <!-- Profile header -->
+    <div ref="horseDetailsElement" class="overflow-y-auto">
+      <article>
+        <!-- Profile header -->
+        <div>
           <div>
-            <div>
-              <div class="h-32 w-full object-cover lg:h-48 banner-svg"></div>
-            </div>
-            <div class="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
-              <div class="-mt-12 sm:-mt-16 sm:items-end">
-                <div class="flex">
-                  <SupabaseImage
-                    v-if="horse.avatar_url"
-                    class="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32 overflow-hidden"
-                    id="horse-avatars"
-                    :path="horse.avatar_url"
-                    alt=""
-                  />
-                  <div
-                    v-else
-                    class="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32 flex items-center justify-center text-white text-6xl"
-                    :class="
-                      horse.avatar_background
-                        ? horse.avatar_background
-                        : 'bg-pink-500'
-                    "
-                  >
-                    {{ horse.name[0].toUpperCase() }}
-                  </div>
-                </div>
-                <div class="mt-6 flex justify-between items-center">
-                  <div>
-                    <h1 class="truncate text-2xl font-bold text-gray-900">
-                      {{ horse.name }}
-                    </h1>
-                  </div>
-
-                  <Menu
-                    as="div"
-                    class="relative"
-                    v-if="
-                      profile &&
-                      ((horse.owner && profile.id === horse.owner.id) ||
-                        profile.active_role < 3)
-                    "
-                  >
-                    <div>
-                      <MenuButton
-                        class="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600"
-                      >
-                        <span class="sr-only">Open options</span>
-                        <EllipsisVerticalIcon
-                          class="h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      </MenuButton>
-                    </div>
-
-                    <transition
-                      enter-active-class="transition ease-out duration-100"
-                      enter-from-class="transform opacity-0 scale-95"
-                      enter-to-class="transform opacity-100 scale-100"
-                      leave-active-class="transition ease-in duration-75"
-                      leave-from-class="transform opacity-100 scale-100"
-                      leave-to-class="transform opacity-0 scale-95"
-                    >
-                      <MenuItems
-                        class="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                      >
-                        <div class="py-1">
-                          <MenuItem v-slot="{ active }">
-                            <button
-                              @click="() => (editModalOpen = true)"
-                              :class="[
-                                active
-                                  ? 'bg-gray-100 text-gray-900'
-                                  : 'text-gray-700',
-                                'block px-4 py-2 text-sm w-full text-left',
-                              ]"
-                            >
-                              Edit
-                            </button>
-                          </MenuItem>
-                          <MenuItem v-slot="{ active }">
-                            <button
-                              @click="() => openDeleteHorseModal()"
-                              :class="[
-                                active
-                                  ? 'bg-gray-100 text-gray-900'
-                                  : 'text-gray-700',
-                                'block px-4 py-2 text-sm w-full text-left',
-                              ]"
-                            >
-                              Delete
-                            </button>
-                          </MenuItem>
-                        </div>
-                      </MenuItems>
-                    </transition>
-                  </Menu>
+            <div class="h-32 w-full object-cover lg:h-48 banner-svg"></div>
+          </div>
+          <div class="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+            <div class="-mt-12 sm:-mt-16 sm:items-end">
+              <div class="flex">
+                <SupabaseImage
+                  v-if="horse.avatar_url"
+                  class="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32 overflow-hidden"
+                  id="horse-avatars"
+                  :path="horse.avatar_url"
+                  alt=""
+                />
+                <div
+                  v-else
+                  class="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32 flex items-center justify-center text-white text-6xl"
+                  :class="
+                    horse.avatar_background
+                      ? horse.avatar_background
+                      : 'bg-pink-500'
+                  "
+                >
+                  {{ horse.name[0].toUpperCase() }}
                 </div>
               </div>
-              <!-- <div class="mt-6 hidden min-w-0 flex-1 sm:block 2xl:hidden">
+              <div class="mt-6 flex justify-between items-center">
+                <div>
+                  <h1 class="truncate text-2xl font-bold text-gray-900">
+                    {{ horse.name }}
+                  </h1>
+                </div>
+
+                <Menu
+                  as="div"
+                  class="relative"
+                  v-if="
+                    profile &&
+                    ((horse.owner && profile.id === horse.owner.id) ||
+                      profile.active_role < 3)
+                  "
+                >
+                  <div>
+                    <MenuButton
+                      class="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600"
+                    >
+                      <span class="sr-only">Open options</span>
+                      <EllipsisVerticalIcon
+                        class="h-6 w-6"
+                        aria-hidden="true"
+                      />
+                    </MenuButton>
+                  </div>
+
+                  <transition
+                    enter-active-class="transition ease-out duration-100"
+                    enter-from-class="transform opacity-0 scale-95"
+                    enter-to-class="transform opacity-100 scale-100"
+                    leave-active-class="transition ease-in duration-75"
+                    leave-from-class="transform opacity-100 scale-100"
+                    leave-to-class="transform opacity-0 scale-95"
+                  >
+                    <MenuItems
+                      class="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    >
+                      <div class="py-1">
+                        <MenuItem v-slot="{ active }">
+                          <button
+                            @click="() => (editModalOpen = true)"
+                            :class="[
+                              active
+                                ? 'bg-gray-100 text-gray-900'
+                                : 'text-gray-700',
+                              'block px-4 py-2 text-sm w-full text-left',
+                            ]"
+                          >
+                            Edit
+                          </button>
+                        </MenuItem>
+                        <MenuItem v-slot="{ active }">
+                          <button
+                            @click="() => openDeleteHorseModal()"
+                            :class="[
+                              active
+                                ? 'bg-gray-100 text-gray-900'
+                                : 'text-gray-700',
+                              'block px-4 py-2 text-sm w-full text-left',
+                            ]"
+                          >
+                            Delete
+                          </button>
+                        </MenuItem>
+                      </div>
+                    </MenuItems>
+                  </transition>
+                </Menu>
+              </div>
+            </div>
+            <!-- <div class="mt-6 hidden min-w-0 flex-1 sm:block 2xl:hidden">
                 <h1 class="truncate text-2xl font-bold text-gray-900">
                   {{ horse.name }}
                 </h1>
               </div> -->
+          </div>
+        </div>
+
+        <!-- Tabs -->
+        <div class="sm:hidden px-4 mt-4">
+          <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
+          <select
+            v-model="selectedTab"
+            id="tabs"
+            name="tabs"
+            class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          >
+            <option v-for="(tab, index) in tabs" :key="tab.name" :value="index">
+              {{ tab.name }}
+            </option>
+          </select>
+        </div>
+        <div class="mt-6 sm:mt-2 2xl:mt-5 hidden sm:block">
+          <div class="border-b border-gray-200">
+            <div class="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
+              <nav class="-mb-px flex space-x-8" aria-label="Tabs">
+                <button
+                  v-for="(tab, index) in tabs"
+                  @click="() => (selectedTab = index)"
+                  :key="tab.name"
+                  :class="[
+                    index == selectedTab
+                      ? 'border-pink-500 text-gray-900'
+                      : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+                    'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm',
+                  ]"
+                  :aria-current="tab.current ? 'page' : undefined"
+                >
+                  {{ tab.name }}
+                </button>
+              </nav>
             </div>
           </div>
+        </div>
 
-          <!-- Tabs -->
-          <div class="sm:hidden px-4 mt-4">
-            <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
-            <select
-              v-model="selectedTab"
-              id="tabs"
-              name="tabs"
-              class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-            >
-              <option
-                v-for="(tab, index) in tabs"
-                :key="tab.name"
-                :value="index"
-              >
-                {{ tab.name }}
-              </option>
-            </select>
-          </div>
-          <div class="mt-6 sm:mt-2 2xl:mt-5 hidden sm:block">
-            <div class="border-b border-gray-200">
-              <div class="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
-                <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-                  <button
-                    v-for="(tab, index) in tabs"
-                    @click="() => (selectedTab = index)"
-                    :key="tab.name"
-                    :class="[
-                      index == selectedTab
-                        ? 'border-pink-500 text-gray-900'
-                        : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                      'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm',
-                    ]"
-                    :aria-current="tab.current ? 'page' : undefined"
-                  >
-                    {{ tab.name }}
-                  </button>
-                </nav>
-              </div>
-            </div>
-          </div>
-
-          <component :is="tabs[selectedTab].component" />
-        </article>
-      </div>
-    </main>
+        <component :is="tabs[selectedTab].component" />
+      </article>
+    </div>
   </div>
 
   <!-- Modals -->
