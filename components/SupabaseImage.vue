654ffaf5c4c802a0ed91d2d7539f2a339753d5b6
KeoneSomers <keone.somers@outlook.com>
2023-03-19 01:36:17 +0000

    more images now using the nuxt-img lib

--- components/SupabaseImage.vue
+++ components/SupabaseImage.vue
@@ -1,35 +1,46 @@
 <script setup>
-  const props = defineProps(["id", "path"]);
-  const { id, path } = toRefs(props);
+const props = defineProps(["id", "path"]);
+const { id, path } = toRefs(props);
 
-  const client = useSupabaseClient();
-  const src = ref("");
+const client = useSupabaseClient();
+const src = ref("");
 
-  onMounted(() => {
-    downloadImage();
+onMounted(() => {
+  downloadImage();
 
-    watchEffect(() => {
-      if (path) {
-        downloadImage();
-      }
-    });
-  });
-
-  const downloadImage = async () => {
-    try {
-      const { data, error } = await client.storage
-        .from(id.value)
-        .download(path.value);
-      if (error) throw error;
-      src.value = URL.createObjectURL(data);
-    } catch (error) {
-      console.error("Error downloading image: ", error.message);
+  watchEffect(() => {
+    if (path) {
+      downloadImage();
     }
-  };
+  });
+});
+
+const downloadImage = async () => {
+  try {
+    const { data, error } = await client.storage
+      .from(id.value)
+      .download(path.value);
+    if (error) throw error;
+    src.value = URL.createObjectURL(data);
+  } catch (error) {
+    console.error("Error downloading image: ", error.message);
+  }
+};
 </script>
 
 <template>
   <div class="flex justify-center items-center bg-pink-500">
-    <img v-if="src" :src="src" alt="Image" class="object-cover" />
+    <!-- <img v-if="src" :src="src" alt="Image" class="object-cover" /> -->
+    <nuxt-img
+      v-if="src"
+      fit="inside"
+      quality="100"
+      :src="src"
+      alt="Image"
+      width="128"
+      height="128"
+      class="object-cover"
+      format="webp"
+    />
   </div>
 </template>
