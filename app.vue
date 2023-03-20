f3b38d48dd0649803d214d5054eaa695a144f31a
KeoneSomers <keone.somers@outlook.com>
2023-03-20 08:32:27 +0000

    added a simple mail "get in touch" button

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { RealtimeChannel } from "@supabase/supabase-js";
 import { ModalsContainer } from "vue-final-modal";
-import { PaperAirplaneIcon } from "@heroicons/vue/24/outline";
+import { PaperAirplaneIcon, EnvelopeIcon } from "@heroicons/vue/24/outline";
 import FeedbackModal from "@/components/modals/FeedbackModal.vue";
 import Alerts from "@/components/Alerts.vue";
 
@@ -86,6 +86,17 @@ onUnmounted(() => {
         :is-open="feedbackModalOpen"
         @close="feedbackModalOpen = false"
       /> -->
+
+      <a
+        href="mailto:keone@yardle.app"
+        class="z-50 inline-flex cursor-pointer fixed top-3 md:top-auto right-16 md:bottom-5 text-sm md:text-base md:right-5 items-center rounded-full border border-transparent bg-pink-400 p-1 px-3 md:p-4 md:px-4 text-white hover:bg-pink-500 focus:outline-none focus:ring-2 focus:ring-pink-300 focus:ring-offset-2"
+      >
+        <EnvelopeIcon
+          class="h-4 w-4 md:h-6 md:w-6 mr-1 md:mr-3"
+          aria-hidden="true"
+        />
+        Get in touch!
+      </a>
     </div>
     <Alerts />
     <ModalsContainer />
