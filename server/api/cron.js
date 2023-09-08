2a227d6c33e75c21995f8deb22e93117766bb448
Keone Somers <Keone.somers@outlook.com>
2023-09-08 09:36:14 +0100

    enabled daily cron job for invoices

--- server/api/cron.js
+++ server/api/cron.js
@@ -1,16 +1,3 @@
-// const handler = async (req, res) => {
-//   // send email reminders
-//   await $fetch("/api/sendEventEmailReminders", {
-//     method: "POST",
-//   });
-
-//   // TODO: generate invoices
-
-//   res.end("Cron Job ran successfully!");
-// };
-
-// export default handler;
-
 export default defineEventHandler(async () => {
   // send email reminders
   console.log("Sending calendar event email reminders...");
@@ -20,11 +7,11 @@ export default defineEventHandler(async () => {
   console.log("Reminders Done!");
 
   // generate invoices
-  // console.log("Generating invoices...");
-  // await $fetch("/api/generateInvoces", {
-  //   method: "POST",
-  // });
-  // console.log("Invoices Done!");
+  console.log("Generating invoices...");
+  await $fetch("/api/generateInvoces", {
+    method: "POST",
+  });
+  console.log("Invoices Done!");
 
   return "Cron Job ran successfully!";
 });
