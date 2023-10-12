3df2b377d93316b07654f1c12962495b0aefc880
Keone Somers <Keone.somers@outlook.com>
2023-10-12 16:10:42 +0100

    futher refactoring of new modal

---
+++ components/ModalHeaderLayout.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps(["title"]);
+const emits = defineEmits(["close"]);
+</script>
+
+<template>
+  <UCard
+    :ui="{
+      base: 'h-full flex flex-col',
+      rounded: '',
+      divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+      body: {
+        base: 'grow',
+      },
+    }"
+  >
+    <template #header>
+      <div class="flex items-center justify-between">
+        <h3
+          class="text-base font-semibold leading-6 text-gray-900 dark:text-white"
+        >
+          {{ props.title }}
+        </h3>
+        <UButton
+          color="gray"
+          variant="ghost"
+          icon="i-heroicons-x-mark-20-solid"
+          class="-my-1"
+          @click="$emit('close')"
+        />
+      </div>
+    </template>
+    <slot />
+  </UCard>
+</template>
--- pages/yard/horses.vue
+++ pages/yard/horses.vue
@@ -255,7 +255,7 @@ const selectHorse = (horseId) => {
     </div>
   </div>
 
-  <!-- Create Horse Modal -->
+  <!-- Add Horse Modal -->
   <UModal
     v-model="isOpen"
     v-if="
@@ -265,33 +265,8 @@ const selectHorse = (horseId) => {
     "
     :fullscreen="mobileMode"
   >
-    <UCard
-      :ui="{
-        base: 'h-full flex flex-col',
-        rounded: '',
-        divide: 'divide-y divide-gray-100 dark:divide-gray-800',
-        body: {
-          base: 'grow',
-        },
-      }"
-    >
-      <template #header>
-        <div class="flex items-center justify-between">
-          <h3
-            class="text-base font-semibold leading-6 text-gray-900 dark:text-white"
-          >
-            Add a horse
-          </h3>
-          <UButton
-            color="gray"
-            variant="ghost"
-            icon="i-heroicons-x-mark-20-solid"
-            class="-my-1"
-            @click="isOpen = false"
-          />
-        </div>
-      </template>
+    <ModalHeaderLayout title="Add a horse" @close="isOpen = false">
       <FormsCreateHorseForm @onSuccess="isOpen = false" />
-    </UCard>
+    </ModalHeaderLayout>
   </UModal>
 </template>
