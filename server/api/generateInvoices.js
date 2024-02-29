450084dc43a058e52ce270e6c7ca896559e4ec25
Keone Somers <Keone.somers@outlook.com>
2024-02-29 15:41:41 +0000

    improvements to the  generate invoices job

--- server/api/generateInvoices.js
+++ server/api/generateInvoices.js
@@ -2,57 +2,64 @@ import { createClient } from "@supabase/supabase-js";
 import { DateTime } from "luxon";
 
 export default defineEventHandler(async (event) => {
-  // Create server client with all permissions (this client can be used by a cron job too!)
   const supabaseUrl = process.env.SUPABASE_URL;
   const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
-  const client = createClient(supabaseUrl, supabaseServiceRoleKey);
+  const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
+  //==================//
+  // DELARE FUNCTIONS //
+  //==================//
+  async function getBillingCycles() {
+    console.log("Running getBillingCycles()");
+    const { data, error } = await supabase
+      .from("yard_billing_cycles")
+      .select("*, yard_id!inner(id, name)");
 
-  console.log("Running generateInvoices cron job");
-
-  // Fetch billingCycles where the next billing date is today
-  const { data: billingCycles, error: errorBillingCycles } = await client
-    .from("yard_billing_cycles")
-    .select("*, yard_id!inner(id, name)");
-
-  if (errorBillingCycles) {
-    console.log("Error in billingCycles");
-    console.log(errorBillingCycles);
-    return;
-  }
-
-  // Loop through each billing cycle (Each Yard)
-  for (const billingCycle of billingCycles) {
-    console.log("Proccessing billing cycle: " + billingCycle.id);
-
-    const endDate = DateTime.fromISO(
-      await $fetch("/api/getNextBillingDate", {
-        method: "POST",
-        body: {
-          billingCycle: billingCycle,
-        },
-      })
-    ).toISODate();
-
-    if (endDate !== DateTime.now().toISODate()) {
-      console.log("Not the next billing date");
-      continue;
+    if (error) {
+      console.log("Error in billingCycles", error);
+      return null;
     }
 
-    const startDate = DateTime.fromISO(
-      await $fetch("/api/getPreviousBillingDate", {
-        method: "POST",
-        body: {
-          offset: 1,
-          nextBillingDate: endDate,
-          billingCycle: billingCycle,
-        },
-      })
-    ).toISODate();
+    return data;
+  }
+  async function getBillingCycleNextBillingDate(billingCycle) {
+    console.log("Running getBillingCycleNextBillingDate()");
 
-    console.log("Start date: " + startDate);
+    const res = await $fetch("/api/getNextBillingDate", {
+      method: "POST",
+      body: {
+        billingCycle: billingCycle,
+      },
+    });
 
-    // get all the service_requests for the billing cycle
-    const { data: serviceRequests, error: errorServiceRequests } = await client
+    if (!res) {
+      console.log("Error in getBillingCycleNextBillingDate");
+      return null;
+    }
+
+    return DateTime.fromISO(res).toISODate();
+  }
+  async function getBillingCycleStartDate(billingCycle) {
+    console.log("Running getBillingCycleStartDate()");
+
+    const res = await $fetch("/api/getPreviousBillingDate", {
+      method: "POST",
+      body: {
+        offset: 1,
+        nextBillingDate: endDate,
+        billingCycle: billingCycle,
+      },
+    });
+
+    if (!res) {
+      console.log("Error in getBillingCycleStartDate");
+      return null;
+    }
+
+    return DateTime.fromISO(res).toISODate();
+  }
+  async function getServiceRequests(billingCycle, startDate, endDate) {
+    console.log("Running getServiceRequests()");
+    const { data, error } = await supabase
       .from("service_requests")
       .select("*, horse_id!inner(id, yard_id, owner)")
       .eq("horse_id.yard_id", billingCycle.yard_id.id)
@@ -62,66 +69,113 @@ export default defineEventHandler(async (event) => {
       .filter("canceled_at", "is", null)
       .not("horse_id.owner", "is", null);
 
-    if (errorServiceRequests) {
-      console.log("Error in serviceRequests");
-      console.log(errorServiceRequests);
+    if (error) {
+      console.log("Error getting Service Requests", error);
+      return null;
+    }
+
+    return data;
+  }
+  async function createInvoice(billingCycle, clientId, startDate, endDate) {
+    console.log("Running createInvoice()");
+    const { data, error } = await supabase
+      .from("invoices")
+      .insert({
+        yard_id: billingCycle.yard_id.id,
+        client_id: clientId,
+        start_date: startDate,
+        end_date: endDate,
+      })
+      .select()
+      .single();
+
+    if (error) {
+      console.log("Error creating invoice", error);
+      return null;
+    }
+
+    return data;
+  }
+
+  //====================//
+  // MAIN FUNCTION CODE //
+  //====================//
+  const billingCycles = await getBillingCycles();
+  if (!billingCycles) {
+    console.log("No billing cycles found");
+    return;
+  }
+
+  // Loop over each billingCycle (1 per yard)
+  for (const billingCycle of billingCycles) {
+    console.log("Proccessing billing cycle: " + billingCycle.id);
+
+    const endDate = await getBillingCycleNextBillingDate(billingCycle);
+    if (!endDate) {
+      console.log("No end date found");
+      continue;
+    }
+    if (endDate !== DateTime.now().toISODate()) {
+      console.log("Not the next billing date");
       continue;
     }
 
-    // Add the serviceRequests to the invoiceItems array (TODO: could be mapped to clean the data up - dont need every field)
-    let invoiceItems = serviceRequests;
+    const startDate = await getBillingCycleStartDate(billingCycle);
+    if (!startDate) {
+      console.log("No start date found");
+      continue;
+    }
 
-    // Create an array of all unique horse owners ids from the invoiceItems
-    const horseOwnersIds = [
+    let requestedServices = await getServiceRequests(
+      billingCycle,
+      startDate,
+      endDate
+    );
+    if (!requestedServices) {
+      console.log("No service requests found");
+      continue;
+    }
+
+    const uniqueClientIds = [
       ...new Set(
-        invoiceItems.map((serviceRequest) => serviceRequest.horse_id.owner)
+        requestedServices.map((serviceRequest) => serviceRequest.horse_id.owner)
       ),
     ];
 
-    console.log("Horse owners ids: " + horseOwnersIds);
-
-    // Create an invoice for each client and link all the appropriate service_requests to the invoice
-    for (const clientId of horseOwnersIds) {
-      // 1. Create an invoice for each client
-      const { data: invoiceData, error: errorInvoiceData } = await client
-        .from("invoices")
-        .insert({
-          yard_id: billingCycle.yard_id.id,
-          client_id: clientId,
-          start_date: startDate,
-          end_date: endDate,
-        })
-        .select()
-        .single();
-
-      if (errorInvoiceData) {
-        console.log("Error in invoiceData");
-        console.log(errorInvoiceData);
+    // Loop over each client (who has made at least 1 service request in this billing cycle)
+    for (const clientId of uniqueClientIds) {
+      // Create an invoice record for the client and capture the response
+      const newInvoice = await createInvoice(
+        billingCycle,
+        clientId,
+        startDate,
+        endDate
+      );
+      if (!newInvoice) {
+        console.log("Error creating invoice");
         continue;
       }
 
-      // Attach the invoice items to this invoice.
-      for (const invoiceItem of invoiceItems) {
-        if (invoiceItem.horse_id.owner === clientId) {
-          invoiceItem.invoice_id = invoiceData.id;
-          invoiceItem.horse_id = invoiceItem.horse_id.id;
-          invoiceItem.horse_name = invoiceItem.horse_id.name;
+      // Loop though all the services that have been requested in this billing cycle (from anyone in the yard)
+      for (const requestedService of requestedServices) {
+        if (requestedService.horse_id.owner === clientId) {
+          requestedService.invoice_id = newInvoice.id;
+          requestedService.horse_id = requestedService.horse_id.id;
+          requestedService.horse_name = requestedService.horse_id.name;
         }
       }
-    } // end of client loop (1 invoice per client (per yard))
+    } // End of client loop
 
     // Insert all the invoice_items records (for this yard)
-    const { error: errorInvoiceItemData } = await client
+    const { error: errorInvoiceItemData } = await supabase
       .from("invoice_items")
-      .insert(invoiceItems.map(({ id, ...item }) => item));
+      .insert(requestedServices.map(({ id, ...item }) => item));
 
     if (errorInvoiceItemData) {
       console.log("Error in invoiceItemData");
       console.log(errorInvoiceItemData);
       return;
     }
-
-    console.log("Invoice items created");
   } // end of billing cycle loop
 
   return { result: "ok" };
